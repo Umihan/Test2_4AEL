a709e90ba900af46bd7de0db99190d20079e21a8
HPUMeran <h.umilietti@alice.it>
2020-04-16 10:47:53 +0200

    Zentrierte Ausgabe

--- test4AEL/test4AEL/Program.cs
+++ test4AEL/test4AEL/Program.cs
@@ -9,15 +9,23 @@ namespace test4AEL
     {
         static void Main(string[] args)
         {
-            Titel("Dies ist ein Test");
+            Titel("TITEL DER ANWENDUNG");
             Console.ReadKey();
         }
+
         static void Titel(string titel)
         {
+            int breite = Console.WindowWidth;
+            int textlen = titel.Length;
+
             Console.Clear();
             for (int i = 0; i < Console.WindowWidth; i++)
             {
                 Console.Write("*");
+            }
+            for (int i = 0; i < (breite-textlen)/2; i++)
+            {
+                Console.Write(" ");
             }
             Console.WriteLine(titel);
             for (int i = 0; i < Console.WindowWidth; i++)
